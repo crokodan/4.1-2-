260673182a26402ce2e41e6fd43c13ef06c9b6d0
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 19:01:18 +0400

    Add files via upload

---
+++ 4.1 Задание 2)/4.1 Задание 2)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1_Задание_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 10;
+            int[] array = new int[size];
+            int[] B = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Введите " + (i + 1) + " элемент матрицы: ");
+                array[i] = int.Parse(Console.ReadLine());
+                B[i] = (array[i] / 10) - (array[i] % 10);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine((i + 1) + "-й элемент массива:\t" + array[i]);
+                Console.WriteLine("Разность цифр, составляющих это число, равна:\t" + B[i] + "\n");
+            }
+        }
+    }
+}
